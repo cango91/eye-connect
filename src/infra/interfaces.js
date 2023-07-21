4a76e42fd3a265d883bbf315b7b42eafac6f442e
cango91 <can.gologlu@gmail.com>
2023-07-21 14:30:27 -0400

    add navigation partial view, define interface for navigation items, refactor header partial

---
+++ src/infra/interfaces.js
@@ -0,0 +1,18 @@
+const iNav = {
+    items: [
+        {
+            text: null,
+            href: null,
+            dropdown: null,
+        },
+        {
+            text: null,
+            href: null,
+            dropdown: [{
+                text: null,
+                href: null,
+            }],
+        },
+    ],
+    active: null,
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { header: {} });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    navigation: {
+      items: [{
+        text: 'Home',
+        href: '#',
+      },
+      {
+        text: 'Login',
+        href: '#'
+      }],
+      active: 'Home',
+    }
+  });
 });
 
 module.exports = router;
