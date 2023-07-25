545fab4c64bbb8eb4c41ed98bab84282d3e15faa
cango91 <can.gologlu@gmail.com>
2023-07-25 16:38:15 -0400

    second attempt to suppress error msg

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' || process.env.NODE_ENV !== 'production' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' && process.env.NODE_ENV !== 'production' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
