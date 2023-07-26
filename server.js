dc3a988d39d36036e931d88f6d99b67d46057bbe
cango91 <can.gologlu@gmail.com>
2023-07-26 01:37:12 -0400

    prod check

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   // res.locals.message = req.app.get('env') === 'development' && process.env.NODE_ENV !== 'production' ? err.message : "An error has occured";
-  res.locals.message = err.message;
+  res.locals.message = process.env.NODE_ENV === 'dev' ? err.message : 'An error has occured';
   // res.locals.error = req.app.get('env') === 'development' && process.env.NODE_ENV !== 'production' ? err : {};
   res.locals.error = req.app.get('env') === 'dev' ? err : {};
   // render the error page
