a7689a880ebe8833d60e07250b4ecfbd36547a4a
cango91 <can.gologlu@gmail.com>
2023-07-28 01:06:04 -0400

    BugFix table pagination

--- public/js/tableHandler.js
+++ public/js/tableHandler.js
@@ -123,7 +123,6 @@ const tableHandler = (
 
    // Update the visibility of the pagination controls and page info text based on the page count
    const updatePaginationVisibility = () => {
-       console.log(fetchOptions);
        prevButton.style.visibility = (fetchOptions.page > 1) ? 'visible' : 'hidden';
        nextButton.style.visibility = (fetchOptions.page < fetchOptions.pageCount) ? 'visible' : 'hidden';
        pageInfo.textContent = `Page ${table.dataset.page} of ${table.dataset.pageCount}`;  // update page info text
