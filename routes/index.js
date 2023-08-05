b851b16570a60fc53e055025c90b6c15ac9695d3
cango91 <can.gologlu@gmail.com>
2023-08-05 18:38:36 -0400

    Add index page instead of redirecting from route, in order to show meta information on IM and social media link shares

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. If logged in, take to portal/ if not, the only unrestricted page is /about */
 router.get('/', function (req, res, next) {
-  if(req.user){
-    res.redirect('/portal');
-  }else{
-    res.redirect('/about');
-  }
+  res.render('index');
 });
 
 module.exports = router;
